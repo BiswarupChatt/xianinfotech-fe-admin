b505cfb3c923b0f31ccd9bfe88f3d9b63ab91773
Biswarup Chatterjee <125545198+BiswarupChatt@users.noreply.github.com>
2025-01-24 13:30:25 +0530

    Update AppRoute.tsx

--- src/routes/AppRoute.tsx
+++ src/routes/AppRoute.tsx
@@ -4,8 +4,7 @@ import UsersList from "../pages/user-list/UsersList";
 export default function AppRoute() {
   return (
     <Routes>  
-      <Route path="/" element={<Navigate to="user-list" />} />
-      <Route path="/user-list" element={<UsersList />} />
+      <Route path="/" element={<UsersList />} />
       <Route path="/trnsaction-list" element={<h2>Transaction List</h2>} />
       <Route path="/video-management" element={<h2>Video Management</h2>} />
       <Route path="/top-receivers-list" element={<h2>Top Recievers List</h2>} />
